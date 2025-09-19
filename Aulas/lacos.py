3d0b77755242eae2ddd669599cca267d010a5570
Bruno Leão <bruno.melosh@gmail.com>
2025-09-19 15:45:50 -0400

    voltando a revisar python

---
+++ Aulas/lacos.py
@@ -0,0 +1,6 @@
+"""
+Aula de laços em Python
+"""
+# Laço for
+for x in range(6):
+    print(f"Valor de x: {x}")
---
+++ Aulas/operadores.py
@@ -0,0 +1,17 @@
+"""
+Treinamento de operadores em Python
+"""
+# inserir o valor de a
+a = int(input("Digite um valor para X: "))
+#inserir o valor de b
+b = int(input("Digite um valor para Y: "))
+
+#verficando que é o maior:
+if a > b:
+    print(f"O maior valor é o de X: {a}")
+elif a < b:
+    print(f"O maior valor é o de Y: {b}")
+else:
+    print("Os valores são iguais")
+
+
